2133957294f034d4bfdc75d0f5eab45d57bffa3f
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-08-14 23:12:17 +0700

    css

---
+++ includes/header.php
@@ -0,0 +1,69 @@
+<div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <h1>Quản lý thư viện</h1>
+
+            </div>
+<?php if($_SESSION['login'])
+{
+?> 
+            <div class="right-div">
+                <a href="logout.php" class="btn btn-danger pull-right">Đăng xuất</a>
+            </div>
+            <?php }?>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+<?php if($_SESSION['login'])
+{
+?>    
+<section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="dashboard.php" class="menu-top-active">Chung</a></li>
+                            <li><a href="issued-books.php">Phiếu mượn</a></li>
+                             <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Tài khoản <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="my-profile.php">Thông tin cá nhân</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <?php } else { ?>
+        <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">                        
+                          
+      <li><a href="index.php">Chung</a></li>
+      <li><a href="index.php#ulogin">User Đăng nhập</a></li>
+                            <li><a href="signup.php">Đăng kí</a></li>
+                         
+                            <li><a href="adminlogin.php">Admin đăng nhập</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <?php } ?>
