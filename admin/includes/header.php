950985e6baefc5fcf0ddbef809547d90b8030bf7
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-08-14 23:15:37 +0700

    css admin

---
+++ admin/includes/header.php
@@ -0,0 +1,64 @@
+<div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <h1>Quản lý thư viện</h1>
+
+            </div>
+
+            <div class="right-div">
+                <a href="logout.php" class="btn btn-danger pull-right">Đăng xuất</a>
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="dashboard.php" class="menu-top-active">Chung</a></li>
+                           
+                            <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Loại sách <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="add-category.php">Thêm loại sách</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-categories.php">Quản lý loại sách</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Tác Giả <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="add-author.php">Thêm mới tác giả</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-authors.php">Quản lý tác giả</a></li>
+                                </ul>
+                            </li>
+ <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Sách <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="add-book.php">Thêm mới sách</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-books.php">Quản lý sách</a></li>
+                                </ul>
+                            </li>
+
+                           <li>
+                                <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown">Phiếu mượn sách <i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="issue-book.php">Thêm mới phiếu mượn</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="manage-issued-books.php">Quản lý phiếu mượn</a></li>
+                                </ul>
+                            </li>
+                             <li><a href="reg-students.php">Độc giả</a></li>
+                    
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
